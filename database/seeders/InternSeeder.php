57dffbfb13f092621084864cde9631292d110161
rabbitmaid <rabbitmaid@proton.me>
2025-12-29 07:54:54 +0100

    Remove some user colums from the interns table

--- database/seeders/InternSeeder.php
+++ database/seeders/InternSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use App\Models\Intern;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class InternSeeder extends Seeder
 {
@@ -13,6 +16,18 @@ class InternSeeder extends Seeder
      */
     public function run(): void
     {
-        
+        $user = User::firstOrCreate([
+            'name' => 'Intern User',
+            'email' => 'itern@example.com',
+            'password' => Hash::make('password'),
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10)
+        ]);
+
+        $user->assignRole('intern');
+
+        Intern::factory()->create([
+           'user_id' => $user
+        ]);
     }
 }

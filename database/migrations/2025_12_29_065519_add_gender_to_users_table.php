0f81c8a32e6215705db1285fcddba2eb5c1f272f
rabbitmaid <rabbitmaid@proton.me>
2025-12-29 07:56:20 +0100

    Add gender column to users table

--- database/migrations/2025_12_29_065519_add_gender_to_users_table.php
+++ database/migrations/2025_12_29_065519_add_gender_to_users_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('interns', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->string('gender')->nullable()->after('email');
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('interns', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('gender');
         });
     }

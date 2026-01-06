8237b1bea180711dbe131f772a978a7373fcc58b
rabbitmaid <rabbitmaid@proton.me>
2026-01-06 16:52:09 +0100

    Modifed the template service

--- app/Services/TemplateService.php
+++ app/Services/TemplateService.php
@@ -7,16 +7,18 @@ use ZipArchive;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Storage;
 
-class TemplateService {
+class TemplateService
+{
 
-    public static function uploadTemplate(string $filePath) {
+    public static function uploadTemplate(string $filePath)
+    {
 
         $zip = new ZipArchive;
         $path = Storage::path($filePath);
         $templateDirectory = config('template.path');
         $fileInfo = [];
 
-        if($zip->open($path) === true) {
+        if ($zip->open($path) === true) {
             $zip->extractTo($templateDirectory);
             $fileInfo['fileName'] = str_replace('/', '',  $zip->getNameIndex(0));
             $zip->close();
@@ -31,37 +33,37 @@ class TemplateService {
     }
 
 
-    public static function deleteTemplate(string $template) {
+    public static function deleteTemplate(string $template)
+    {
 
         $templateDirectory = config('template.path');
-        
+
         // Check if template directory exist
-        if(is_dir($templateDirectory)) {
+        if (is_dir($templateDirectory)) {
 
             // Get all template folders within the directory
-            if ($directoryHandle = opendir($templateDirectory)){
+            if ($directoryHandle = opendir($templateDirectory)) {
 
-                    while (($templateFolder = readdir($directoryHandle)) !== false){
-                        
-                        // get folders except the root and the previous dir strings
-                        if(!in_array($templateFolder,['.', '..'])) {
+                while (($templateFolder = readdir($directoryHandle)) !== false) {
 
-                            if($templateFolder === $template) {
-                                return self::recursiveDelete("$templateDirectory/$templateFolder");
-                            }
-                            
+                    // get folders except the root and the previous dir strings
+                    if (!in_array($templateFolder, ['.', '..'])) {
+
+                        if ($templateFolder === $template) {
+                            return self::recursiveDelete("$templateDirectory/$templateFolder");
                         }
                     }
+                }
             }
         }
 
         return false;
     }
 
-    
-    private static function recursiveDelete(string $dir) 
+
+    private static function recursiveDelete(string $dir)
     {
-        $files = array_diff(scandir($dir), array('.','..'));
+        $files = array_diff(scandir($dir), array('.', '..'));
 
         foreach ($files as $file) {
             (is_dir("$dir/$file")) ? self::recursiveDelete("$dir/$file") : unlink("$dir/$file");
@@ -75,57 +77,52 @@ class TemplateService {
     {
         $templateDirectory = config('template.path');
         $templates = new Collection;
-        
+
         // Check if template directory exist
-        if(is_dir($templateDirectory)) {
+        if (is_dir($templateDirectory)) {
 
             // Get all template folders within the directory
-            if ($directoryHandle = opendir($templateDirectory)){
+            if ($directoryHandle = opendir($templateDirectory)) {
 
-                    while (($templateFolder = readdir($directoryHandle)) !== false){
-                        
-                        // get folders except the root and the previous dir strings
-                        if(!in_array($templateFolder,['.', '..'])) {
-    
-                          
-                            $templateFolderContent = "$templateDirectory/$templateFolder";
-                            
-                            // get contents of each individual template directory
-                            // check if the info.json file exist
-    
-                            if(file_exists("$templateFolderContent/info.json")) {
-                                $templateInfo = file_get_contents("$templateFolderContent/info.json");
-                                $templateInfo = json_decode($templateInfo, true);
-    
-                                // get content of info.json and save in the templates collection
-                                $templateInfo["path"] = $templateFolderContent;
-                                $templateInfo["slug"] = $templateFolder;
-    
-                                if(array_key_exists("screenshot", $templateInfo)) {
-                                    if(empty($templateInfo['screenshot'])) {
-                                        $templateInfo['screenshot'] = "no-screenshot.png";
-                                    }
+                while (($templateFolder = readdir($directoryHandle)) !== false) {
+
+                    // get folders except the root and the previous dir strings
+                    if (!in_array($templateFolder, ['.', '..'])) {
+
+
+                        $templateFolderContent = "$templateDirectory/$templateFolder";
+
+                        // get contents of each individual template directory
+                        // check if the info.json file exist
+
+                        if (file_exists("$templateFolderContent/info.json")) {
+                            $templateInfo = file_get_contents("$templateFolderContent/info.json");
+                            $templateInfo = json_decode($templateInfo, true);
+
+                            // get content of info.json and save in the templates collection
+                            $templateInfo["path"] = $templateFolderContent;
+                            $templateInfo["slug"] = $templateFolder;
+
+                            if (array_key_exists("screenshot", $templateInfo)) {
+                                if (empty($templateInfo['screenshot'])) {
+                                    $templateInfo['screenshot'] = "no-screenshot.png";
                                 }
-    
-                                $templates->add($templateInfo);
-    
                             }
-                            else {
-                                throw new Exception("Certificate template has no info.json file");
-                            }
-    
+
+                            $templates->add($templateInfo);
+                        } else {
+                            throw new Exception("Certificate template has no info.json file");
                         }
                     }
-    
-                    // close handle when done
-                    closedir($directoryHandle);
+                }
 
-            }else {
+                // close handle when done
+                closedir($directoryHandle);
+            } else {
                 throw new Exception("Unexpected error while accessing the template directory");
             }
-        }
-        else {
-             throw new Exception("No certificate template directory available");
+        } else {
+            throw new Exception("No certificate template directory available");
         }
 
         return $templates;
@@ -135,8 +132,7 @@ class TemplateService {
     {
         $templates = self::getTemplates();
         $template = $templates->where('slug', $slug)->first();
-       
+
         return $template;
     }
-
 }
--- resources/views/dashboard/templates/create.blade.php
+++ resources/views/dashboard/templates/create.blade.php
@@ -1,17 +1,21 @@
 <x-app-layout pageTitle="Upload Certificate Template">
 
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Upload Certificate Template') }}
-        </h2>
+        <div class="flex items-center justify-between">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Upload Certificate Template') }}
+            </h2>
+
+            <x-link-button href="{{ route('template.index') }}">Back </x-link-button>
+        </div>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-    
+
             <div class="flex justify-between items-center mb-6">
                 <h1 class="text-3xl font-bold text-primary">Upload</h1>
-    
+
                 <a href="#" class="px-4 py-1.5 text-sm bg-primary hover:bg-primary/90 text-white rounded-lg font-semibold transition">
                     Back
                 </a>
@@ -19,33 +23,33 @@
 
             <div class="mt-8 bg-white shadow rounded-lg p-6 space-y-5">
                 <h3 class="font-semibold">Important Notice for Uploads!!</h3>
-                    
+
                 <ul class="list-disc ms-8 text-sm leading-relaxed">
                     <li>You cannot upload templates with thesame name more than once</li>
                     <li>Upload the zip file containing the template design and assets</li>
                     <li>Download the sample for more details</li>
-                </ul>    
+                </ul>
             </div>
-    
+
             <div class="bg-white shadow-lg rounded-lg p-8 mt-8">
 
-               <div class="max-w-xl mb-5 gap-5 flex flex-col md:flex-row items-center justify-between">
-                 <h2 class="text-lg font-semibold">Upload Template</h2>
-                 <a class="px-4 py-1.5 text-sm bg-blue-600 hover:bg-blue-600/90 text-white rounded-lg font-semibold transition" href="{{ asset('sample/tickets.csv') }}">Download Sample</a>
-               </div>
-    
+                <div class="max-w-xl mb-5 gap-5 flex flex-col md:flex-row items-center justify-between">
+                    <h2 class="text-lg font-semibold">Upload Template</h2>
+                    <a class="px-4 py-1.5 text-sm bg-blue-600 hover:bg-blue-600/90 text-white rounded-lg font-semibold transition" href="{{ asset('sample/tickets.csv') }}">Download Sample</a>
+                </div>
+
                 <div class="max-w-xl">
                     <form action="{{ route('template.upload') }}" method="POST" enctype="multipart/form-data">
                         @csrf
                         <input type="file" name="file" accept=".zip" required class="border p-2 rounded block w-full">
-                         <x-input-error :messages="$errors->get('file')" class="mt-2" />
+                        <x-input-error :messages="$errors->get('file')" class="mt-2" />
 
                         {{-- Submit --}}
                         <div class="pt-4">
                             <x-primary-button class="w-full justify-center">{{ __('Upload') }}</x-primary-button>
                         </div>
                     </form>
-                  
+
                 </div>
             </div>
 

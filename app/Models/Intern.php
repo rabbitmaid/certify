aef52dad8eff453be0b97c767ab49d04d7a5b9e4
rabbitmaid <rabbitmaid@proton.me>
2026-02-22 21:30:47 +0100

    Create pivot table for interns and internship sessions

--- app/Models/Intern.php
+++ app/Models/Intern.php
@@ -2,23 +2,35 @@
 
 namespace App\Models;
 
+use App\Models\InternshipSession;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Intern extends Model
 {
     use HasFactory, SoftDeletes;
     
-    protected $fillable = ["user_id", "matricule", "salutation", "id_card_number", "phone_number", "school", "diploma", "department", "level",  "duration", "start_date", "end_date", "date_of_birth", "language", "other_information", "status", "rejection_reason"];
+    protected $fillable = ["user_id", "matricule", "salutation", "id_card_number", "phone_number", "school", "diploma", "department", "level",  "duration", "start_date", "end_date", "date_of_birth", "language", "other_information", "status", "rejection_reason" , 'approved_at', 'rejected_at' ,'unapproved_at'];
 
     protected $casts = [
         "start_date" => "date",
-        "end_date" => "date"
+        "end_date" => "date",
+        "date_of_birth" => "date",
+        "approved_at" => "datetime",
+        "rejected_at" => "datetime",
+        "unapproved_at" => "datetime"
     ];
 
 
-    public function user() {
+    public function user() 
+    {
         return $this->belongsTo(User::class);
+    }
+
+    public function internSessions(): BelongsToMany
+    {
+        return $this->belongsToMany(InternshipSession::class);
     }
 }
--- app/Models/InternshipSession.php
+++ app/Models/InternshipSession.php
@@ -3,8 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class InternshipSession extends Model
 {
-    //
+     protected $fillable = [
+        'title',
+        'start_date',
+        'end_date',
+        'status',
+        'description',
+    ];
+
+    protected $casts = [
+        'start_date' => 'date',
+        'end_date' => 'date',
+    ];
+
+    public function interns() : BelongsToMany
+    {
+        return $this->belongsToMany(Intern::class);
+    }
 }
